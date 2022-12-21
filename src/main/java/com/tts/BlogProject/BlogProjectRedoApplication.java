c2945178f0c3f20fbe306339f1bb368bd8d0bb50
Jonathan Zeigler <zeigler.jonathan@gmail.com>
2022-12-21 13:08:54 -0800

    first commit

---
+++ src/main/java/com/tts/BlogProject/BlogProjectRedoApplication.java
@@ -0,0 +1,14 @@
+package com.tts.BlogProject;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectRedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProjectRedoApplication.class, args);
+	}
+
+}
